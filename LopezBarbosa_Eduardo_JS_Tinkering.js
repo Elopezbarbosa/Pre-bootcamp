8d92d972b99ea19c5b1ba9b6f1b806c41e24fc38
Eduardo Lopez Barbosa <96999868+Elopezbarbosa@users.noreply.github.com>
2022-01-04 16:23:10 -0800

    Add files via upload

---
+++ LopezBarbosa_Eduardo_JS_Tinkering.js
@@ -0,0 +1,9 @@
+var childHeight = [60];
+function displayIfChildIsAblezToRideTheRollerocoster () {
+    if (childHeight > 53) {
+        console.log ("Sorry kiddo. Maybe next year.")
+    }
+    else {
+        console.log ("Get on that ride, Kiddo!")
+    }
+}
